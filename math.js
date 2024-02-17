8403d70c25d9a865827450c256f0445701de55da
MartaAlbert <159722939+MartaAlbert@users.noreply.github.com>
2024-02-17 20:08:34 +0100

    Update math.js

--- math.js
+++ math.js
@@ -6,7 +6,15 @@ function restar(a,b) {
     return a-b;
 }
 
+function parell_senar(a) {
+    if (a%2==0){
+        return true;
+    }else{
+        return false;
+}
+
 module.exports = {
     sumar,
-    restar
+    restar,
+    parell_senar
 }
